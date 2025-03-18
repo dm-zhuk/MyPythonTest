3d644541868df4e293c8b90456ee959e159b5619
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2025-03-19 00:16:44 +0200

    finished tests

--- topic_10/tests.py
+++ topic_10/tests.py
@@ -11,7 +11,7 @@ class Point:
 
     @x.setter
     def x(self, x):
-        if isinstance(x, (int, float)):
+        if (type(x) == int) or (type(x) == float):
             self.__x = x
 
     @property
@@ -20,11 +20,11 @@ class Point:
 
     @y.setter
     def y(self, y):
-        if isinstance(y, (int, float)):
+        if (type(y) == int) or (type(y) == float):
             self.__y = y
 
     def __str__(self):
-        return f"Point({self.x}, {self.y})"
+        return f"Point({self.x},{self.y})"
 
 
 class Vector:
@@ -34,26 +34,71 @@ class Vector:
     def __setitem__(self, index, value):
         if index == 0:
             self.coordinates.x = value
-        elif index == 1:
+        if index == 1:
             self.coordinates.y = value
 
     def __getitem__(self, index):
         if index == 0:
             return self.coordinates.x
-        elif index == 1:
+        if index == 1:
             return self.coordinates.y
 
+    def __call__(self, value=None):
+        if value:
+            self.coordinates.x = self.coordinates.x * value
+            self.coordinates.y = self.coordinates.y * value
+        return self.coordinates.x, self.coordinates.y
+
+    def __add__(self, vector):
+        x = self.coordinates.x + vector.coordinates.x
+        y = self.coordinates.y + vector.coordinates.y
+        return Vector(Point(x, y))
+
+    def __sub__(self, vector):
+        x = self.coordinates.x - vector.coordinates.x
+        y = self.coordinates.y - vector.coordinates.y
+        return Vector(Point(x, y))
+
+    def __mul__(self, vector):
+        return (
+            self.coordinates.x * vector.coordinates.x
+            + self.coordinates.y * vector.coordinates.y
+        )
+
+    def len(self):
+        return (self.coordinates.x**2 + self.coordinates.y**2) ** 0.5
+
     def __str__(self):
-        return f"Vector({self.coordinates.x}, {self.coordinates.y})"
+        return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+    def __eq__(self, vector):
+        return self.len() == vector.len()
+
+    def __ne__(self, vector):
+        return self.len() != vector.len()
+
+    def __lt__(self, vector):
+        return self.len() < vector.len()
+
+    def __gt__(self, vector):
+        return self.len() > vector.len()
+
+    def __le__(self, vector):
+        return self.len() <= vector.len()
+
+    def __ge__(self, vector):
+        return self.len() >= vector.len()
 
 
-if __name__ == "__main__":
-    point = Point(1, 10)
-    vector = Vector(point)
+vector1 = Vector(Point(1, 10))
+vector2 = Vector(Point(3, 10))
 
-    print(point)
-    print(vector)
-
+print(vector1 == vector2)  # Output: False
+print(vector1 != vector2)  # Output: True
+print(vector1 > vector2)  # Output: False
+print(vector1 < vector2)  # Output: True
+print(vector1 >= vector2)  # Output: False
+print(vector1 <= vector2)  # Output: True
 
 """
 import pickle
